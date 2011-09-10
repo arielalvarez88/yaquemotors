dae3554e5061a1361bb38f38b5beefd8f495c5ea
Ariel <ariel@ariel-desktop.(none)>
2011-09-10 06:12:06 -0400

    adding the fotos for post-venta

---
+++ includes/blocks/postSell.php
@@ -0,0 +1,30 @@
+<?php require dirname(__FILE__).'/../paths.php';?>
+<div id="post-sell">
+    <div id="post-sell-workshop-services">
+        <a id="post-sell-workshop-services-title" href="/taller-de-servicios">TALLER DE SERVICIOS<img class="post-sell-white-arrow" src="<?php echo $paths->images;?>/common/whiteArrow.png"/></a>
+        <a id="post-sell-workshop-services-photo" href="/taller-de-servicios"></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices dui a quam vulputate sit amet venenatis nisl tempus. Sed at felis lectus. Pellentesque in mauris risus. </p>
+        
+        
+    </div>    
+
+
+
+    <div id="post-sell-workshop-services-divisor" class="post-sell-divisor">
+        <?php require dirname(__FILE__) . '/bigVerticalDivisor.php'; ?>
+    </div>
+    
+    <div class="post-sell-divisor">
+        <?php require dirname(__FILE__) . '/bigVerticalDivisor.php'; ?>
+    </div>
+    
+    <div id="post-sell-workshop-parts-store">
+
+        <a id="post-sell-workshop-parts-store-title" href="/tienda-de-repuestos">TIENDA DE REPUESTOS<img class="post-sell-white-arrow" src="<?php echo $paths->images;?>/common/whiteArrow.png"/></a>
+        <a id="post-sell-workshop-parts-store-photo" href="/tienda-de-repuestos"></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices dui a quam vulputate sit amet venenatis nisl tempus. Sed at felis lectus. Pellentesque in mauris risus. </p>
+
+    </div>    
+
+
+</div>
