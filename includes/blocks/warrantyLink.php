5595a269be870ddbcec56f677a4497ddf7a14c4e
Ariel <ariel@ariel-desktop.(none)>
2011-09-23 18:24:31 -0400

    all js and photos

---
+++ includes/blocks/warrantyLink.php
@@ -0,0 +1,4 @@
+<?php require dirname(__FILE__).'/../paths.php';?>
+<a href="/vehiculos-con-garantia" id="warranty-link">
+    <img lat="foto-link-grarantia" src="<?php echo $paths->images;?>/warrantyLink/warrantyVehicle.png"/>
+</a>
