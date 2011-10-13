3fce9106960f2aaef95fd5f200b39a25b7d99957
Ariel <ariel@ariel-desktop.(none)>
2011-10-13 16:28:05 -0400

    correct emails

--- includes/formEmails.php
+++ includes/formEmails.php
@@ -1,29 +1,20 @@
 <?php
 
-//$emails['parts'] = "centroservicios@elyaquemotors.com";
-//$emails['partsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "CC: j.guillen@elyaquemotoros.com";
+$emails['parts'] = "centroservicios@elyaquemotors.com";
+$emails['partsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "Cc: j.guillen@elyaquemotoros.com";
 
-$emails['parts'] = "arielalvarez88@hotmail.com";
-$emails['partsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "Cc: arielalvarez88@gmail.com";
 
-//$emails['appointments'] = "centroservicios@elyaquemotors.com";
-//$emails['appointmentsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "CC: jm.suarez@elyaquemotors.com";
+$emails['appointments'] = "centroservicios@elyaquemotors.com";
+$emails['appointmentsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "Cc: jm.suarez@elyaquemotors.com";
 
-$emails['appointments'] = "arielalvarez88@hotmail.com";
-$emails['appointmentsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>' . "\r\n" . "Cc: arielalvarez88@gmail.com";
 
-//$emails['contacts'] = "ventas@elyaquemotors.com";
-//$emails['contactsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>';
-
-$emails['contacts'] = "arielalvarez88@hotmail.com";
+$emails['contacts'] = "ventas@elyaquemotors.com";
 $emails['contactsHeaders'] = 'From: Web - elyaquemotors <noreply@elyaquemotors.com>';
 
-//$emails['cotizar'] = "centroservicios@elyaquemotoros.com";
-//$emails['cotizarHeaders'] = "From: Web - elyaquemotors <noreply@elyaquemotors.com>";
 
-$emails['cotizar'] = "arielalvarez88@hotmail.com";
+
+$emails['cotizar'] = "centroservicios@elyaquemotoros.com";
 $emails['cotizarHeaders'] = "From: Web - elyaquemotors <noreply@elyaquemotors.com>";
-
 
 
 ?>
