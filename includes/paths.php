2a1809fb3231453d9a131f5850566a4d39db2d8b
Ariel <ariel@ariel-desktop.(none)>
2011-12-08 17:15:01 -0400

    add paths

---
+++ includes/paths.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of paths
+ *
+ * @author ariel
+ */
+
+
+
+$paths->webpage = 'http://local.yaquemotors.com';
+$paths->images = '/sites/default/files/images';
+$paths->files = '/sites/default/files';
+
+?>
