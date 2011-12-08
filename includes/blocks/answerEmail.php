f682d178fa97f9bad3df32d2543fc5d65aae2166
Ariel <ariel@ariel-desktop.(none)>
2011-12-08 17:09:11 -0400

    removing include dompdf

--- includes/blocks/answerEmail.php
+++ includes/blocks/answerEmail.php
@@ -5,7 +5,7 @@
  */
 require dirname(__FILE__) . '/../paths.php';
 require dirname(__FILE__) . '/../classes/email_helper.php';
-require dirname(__FILE__) . '/../classes/dompdf/dompdf_config.inc.php';
+
 require dirname(__FILE__) . '/../classes/tcpdf/tcpdf.php';
 
 
