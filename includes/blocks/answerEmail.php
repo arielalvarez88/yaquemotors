d947a3e67098911f33bc445d20ffa28e552a03a1
Ariel <ariel@ariel-desktop.(none)>
2011-12-09 18:04:47 -0400

    removing pdf

--- includes/blocks/answerEmail.php
+++ includes/blocks/answerEmail.php
@@ -6,7 +6,7 @@
 require dirname(__FILE__) . '/../paths.php';
 require dirname(__FILE__) . '/../classes/email_helper.php';
 require dirname(__FILE__) . '/../formEmails.php';
-
+require dirname(__FILE__) . '/../classes/dompdf/dompdf_config.inc.php';
 require dirname(__FILE__) . '/../classes/tcpdf/tcpdf.php';
 
 
@@ -17,23 +17,7 @@ if (isset($_POST['client-name'])) {
     <html>
   <head>
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    
-    
-        <style type="text/css">   
-
-
-
-#answer-email-template-prices p {
-    margin-bottom: 0;
-}
-
-
-#answer-email-template-warning
-{
-    
-    
-}
-    </style>
+                
     </head>
 
     <body>
@@ -95,64 +79,6 @@ EOD;
 
             
 $success = mail($_POST['client-email'], "Respuesta Cotización", $html, $emails['cotizarResponseHeaders']);
-
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 021');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 021', PDF_HEADER_STRING);
-
-// set header and footer fonts
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-//set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-//set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-//set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-//set some language-dependent strings
-$pdf->setLanguageArray($l);
-
-// ---------------------------------------------------------
-
-// set font
-$pdf->SetFont('helvetica', '', 9);
-
-// add a page
-$pdf->AddPage();
-
-
-
-
-// output the HTML content
-$pdf->writeHTML($html, true, 0, true, 0);
-
-// reset pointer to the last page
-$pdf->lastPage();
-
-// ---------------------------------------------------------
-
-//Close and output PDF document
-$pdf->Output('example_021.pdf', 'I');
-
-
-
 
 
 if($success)
