6cc2fa484b3ab8a2ebc5c80e68cd77a9195fa944
Ariel <ariel@ariel-desktop.(none)>
2012-01-27 10:59:13 -0400

    google analitics

--- sites/all/themes/basic/templates/page.tpl.php
+++ sites/all/themes/basic/templates/page.tpl.php
@@ -18,6 +18,20 @@
         <!--[if IE 7]><style type="text/css" media="all">@import "<?php print $base_path . path_to_theme() ?>/css/ie7.css"</style><![endif]-->
         <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
         <?php print $scripts; ?>
+        
+        <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28697833-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     </head>
 
     <body class="<?php print $body_classes; ?> tk-purista-web">
